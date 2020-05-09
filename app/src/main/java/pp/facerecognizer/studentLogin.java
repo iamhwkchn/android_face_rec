fe3e0c8dfc17f7436c78ced34c3a659b3476d488
sriram <sriramkrypto@gmail.com>
2020-05-09 14:36:47 +0530

    Initial commit

---
+++ app/src/main/java/pp/facerecognizer/studentLogin.java
@@ -0,0 +1,14 @@
+package pp.facerecognizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class studentLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_login);
+    }
+}
